61df274af6f8356ade96b034416617e4a8a87edd
R8008 <114330480+R8008@users.noreply.github.com>
2022-09-27 02:51:16 +0900

    Update m5-1.php

--- m5-1.php
+++ m5-1.php
@@ -8,9 +8,9 @@
         
         
 <?php
-    $dsn='*******';
-    $user='********';
-    $password="*******";
+    $dsn='データベース名';
+    $user='ユーザーネーム名';
+    $password="パスワード";
     $pdo=new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING));
        
     $sql="CREATE TABLE IF NOT EXISTS m51"."(
